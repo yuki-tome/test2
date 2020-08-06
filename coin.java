3157947505031a26c1d34f82c006b472031ec9d0
當銘雄紀 <yuki@toumeyuukinoMBP>
2020-08-06 17:09:52 +0900

    HoT: Initial revision

---
+++ coin.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class coin{
+  public static void main(String[] args){
+    String[] sides = {"Heads", "Tails"};
+    Random r = new Random();
+    int hd = 0;
+    int tl = 0;
+
+    System.out.println("Tossing the dice...");
+    for(int i = 0; i < 3; i++){
+        int num = r.nextInt(2);
+        String side = sides[num];
+        int a = i+1;
+        System.out.println("Round " + a + ":" + side);
+        if(num == 1) tl++;
+        else hd++;
+    }
+    System.out.println("Heads: " + hd + "," + "Tails: " + tl );
+   }
+
+  }
+}
